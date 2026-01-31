c805a077a6d623760c19ea6ca61e03476a541553
aDiamant03 <dva01082009@mail.ru>
2026-01-31 10:13:51 +0300

    Initial project skeleton

---
+++ src/main/kotlin/school/project/teamproject/ClubLearnApplication.kt
@@ -0,0 +1,11 @@
+package school.project.teamproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClubLearnApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClubLearnApplication>(*args)
+}
